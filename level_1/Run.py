d43df7d22eac5113285a7a5f30d43314f0fa1946
majdi14789 <majdi.saadani@gmail.com>
2019-11-14 12:03:43 +0100

    LEVEL_1

---
+++ level_1/Run.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+
+from DataChanllenge import DataChallengeHandler
+
+data_challenge_handler = DataChallengeHandler()
+
+
+
+in_folder = "D:\\lifen\\data-jobs\\communications"
+out_folder = "D:\\lifen\\data-jobs\\processed"
+print(data_challenge_handler.proceed_communication(in_folder, out_folder))
